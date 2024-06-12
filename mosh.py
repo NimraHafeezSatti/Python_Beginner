6fc08d38d40e30258170ef66cdd31ba3ec22518e
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-12 23:55:17 +0500

    type-conversion_2

--- mosh.py
+++ mosh.py
@@ -4,9 +4,12 @@
 
 # print( name  + " is "  +   favorite_color)
 # type conversion
-birth_year = input("Birth year: ")
-
-print(type(birth_year))
-age = 2024 - int(birth_year)
-print(type(age))
-print(age)
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2024 - int(birth_year)
+# print(type(age))
+# print(age)
+# ask a user for weight in pounds and convert weight to kg
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
