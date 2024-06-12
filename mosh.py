c1b4e9008b945eb2288706bce02e0889b18fbc02
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-12 23:39:52 +0500

    type-conversion-1

--- mosh.py
+++ mosh.py
@@ -3,7 +3,10 @@
 # favorite_color = input("what is your favorite color")
 
 # print( name  + " is "  +   favorite_color)
-
+# type conversion
 birth_year = input("Birth year: ")
+
+print(type(birth_year))
 age = 2024 - int(birth_year)
+print(type(age))
 print(age)
