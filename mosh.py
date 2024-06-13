e246ea1764b4ea495210e432f819ddaddef48ccc
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-13 23:46:17 +0500

    we use square brackets to get a character and a given index in this string

--- mosh.py
+++ mosh.py
@@ -14,16 +14,19 @@
 # weight_kg = int(weight_lbs) * 0.45
 # print(weight_kg)
 # strings
-# course='python for "beginners"'
+# course='python for beginners' 
 # print(course)
 # strings_1
-course='''
-Hi john,
+# course='''
+# Hi john,
 
-Here is you first email.
+# Here is you first email.
 
-Thank you,
-The support team
+# Thank you,
+# The support team
 
-'''
-print(course)
+# '''
+# print(course)
+#we use square brackets to get a character and a given index in this string
+course='python for bignners'
+print(course[-3])
