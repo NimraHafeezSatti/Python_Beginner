08fd29048c8c580e1840be9b91b690909147d03d
Nimra-Python-Developer <nimrahafeez2007@gmail.com>
2024-06-27 17:22:16 +0500

    logical opeartors

--- mosh.py
+++ mosh.py
@@ -35,5 +35,11 @@
 #course='python for bignners'
 #print(course[0:3])
 #we use square brackets to get a character and a given index in this string(3)
-course='python for beginners'
-print(course[0:])
+#course='python for beginners'
+#print(course[0:])
+#formatted string
+#logical opearator
+has_high_income=True
+has_good_credits=True
+if has_high_income and has_good_credits:
+        print("eligible for loan")
